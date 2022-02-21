37e25cef06b37e48e460f9d6ff49ac5cdad180e1
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-02-21 20:42:14 +0600

    Сделал основное задание

--- src/app/keypad.actions.ts
+++ src/app/keypad.actions.ts
@@ -1,11 +1,13 @@
 import { createAction, props } from '@ngrx/store';
 
-export const reset = createAction('[Keypad] Reset');
-
 export const addChar = createAction(
   '[Keypad] Add character',
   props<{char: string}>()
 );
+
+export const remove = createAction('[Keypad] Remove');
+
+
 
 
 
--- src/app/keypad.reducer.ts
+++ src/app/keypad.reducer.ts
@@ -1,26 +1,41 @@
 import { createReducer, on } from '@ngrx/store';
-import { addChar } from './keypad.actions';
+import { addChar, remove } from './keypad.actions';
 
 const correctPassword = '1337';
-const initialState = {
+
+export interface Key {
+  isCorrect: boolean,
+  password: string
+}
+
+const initialState: Key = {
   isCorrect: false,
   password: ''
 };
 
 export const keypadReducer = createReducer(
   initialState,
+
   on(addChar, (state, {char}) => {
     if (state.password.length >= 4) {
       return state;
     }
-
     const newPassword = state.password + char;
     const newState = {...state, password: newPassword};
 
     if (newPassword === correctPassword) {
-      newState.isCorrect = true
+      newState.isCorrect = true;
     }
 
+    return newState;
+  }),
+
+  on(remove, (state) => {
+    const newPassword = state.password.substr(0,state.password.length - 1);
+    const newState = {...state, password: newPassword};
+    if (newPassword !== correctPassword) {
+      newState.isCorrect = false;
+    }
     return newState;
   }),
 );
